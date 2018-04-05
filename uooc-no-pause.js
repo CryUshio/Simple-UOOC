279f59de80282dc34c1e61392a577582631d8c66
黄铖浩 <30655354+chym123@users.noreply.github.com>
2018-04-05 16:55:23 +0800

    Update uooc-no-pause.js

--- uooc-no-pause.js
+++ uooc-no-pause.js
@@ -3,6 +3,9 @@ jQuery(function($) {
         $('#player_html5_api').on({
           pause: function() {
             $('#player_html5_api').trigger('play');
+          },
+          ended: function() {
+            alert('当前视频已经播放完毕！')
           }
         })
     })
