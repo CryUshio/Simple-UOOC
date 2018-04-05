110b861b9ad4275fc210a66023fd3841c2e4e127
黄铖浩 <30655354+chym123@users.noreply.github.com>
2018-04-05 17:31:20 +0800

    Update uooc-no-pause.js
    添加静音和变速

--- uooc-no-pause.js
+++ uooc-no-pause.js
@@ -1,5 +1,8 @@
 jQuery(function($) {
-    $('body').click(function(){
+    $('.vjs-big-play-button.animated.fadeIn').click(function(){
+        $("div.vjs-volume-menu-button").click(); //静音
+        $(".vjs-playback-rate ul[class='vjs-menu-content'] li")[0].click(); //变速
+        
         $('#player_html5_api').on({
           pause: function() {
             $('#player_html5_api').trigger('play');
