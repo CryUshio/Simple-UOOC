74e203a3321bcae9fc6e86e2e7c6e49fb7bd3b10
黄铖浩 <30655354+chym123@users.noreply.github.com>
2018-04-05 17:59:06 +0800

    Update uooc-no-pause.js
    bug fix

--- uooc-no-pause.js
+++ uooc-no-pause.js
@@ -1,15 +1,20 @@
 jQuery(function($) {
-    $('.vjs-big-play-button.animated.fadeIn').click(function(){
-        $("div.vjs-volume-menu-button").click(); //静音
-        $(".vjs-playback-rate ul[class='vjs-menu-content'] li")[0].click(); //变速
-        
-        $('#player_html5_api').on({
-          pause: function() {
-            $('#player_html5_api').trigger('play');
-          },
-          ended: function() {
-            alert('当前视频已经播放完毕！')
-          }
-        })
+    $('body').on({
+        click: function(){
+            $('.vjs-big-play-button.animated.fadeIn').click(function(){
+                $(".vjs-playback-rate ul[class='vjs-menu-content'] li")[0].click(); //变速
+                $("div.vjs-volume-menu-button").click(); //静音
+            })
+        },
+        mouseout: function(){
+             $('#player_html5_api').on({
+                  pause: function() {
+                    $('#player_html5_api').trigger('play');
+                  },
+                  ended: function() {
+                    alert('当前视频已经播放完毕！')
+                  }
+            })   
+        }
     })
 });
