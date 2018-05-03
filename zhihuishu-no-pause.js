1c382e43cf76f6ef50697a6f807161d6dfca9b33
黄铖浩 <30655354+chym123@users.noreply.github.com>
2018-05-03 11:26:38 +0800

    Update zhihuishu-no-pause.js

--- zhihuishu-no-pause.js
+++ zhihuishu-no-pause.js
@@ -2,15 +2,15 @@ jQuery(function($) {
 	console.log('load');
 	$('li').click(function(e){
 		console.log('listening'); 
-			$("div.speedTab15").click(); //变速
-			$("div.volumeIcon").click(); //静音
-			$('#vjs_mediaplayer_html5_api').on({
-			pause: function() {
-				console.log('continue'); $('#vjs_mediaplayer_html5_api').trigger('play');
-			},
-			ended: function() {
-				alert('当前视频已经播放完毕！')
-			} 
+		$("div.speedTab15").click(); //变速
+		$("div.volumeIcon").click(); //静音
+		$('#vjs_mediaplayer_html5_api').on({
+		pause: function() {
+			console.log('continue'); $('#vjs_mediaplayer_html5_api').trigger('play');
+		},
+		ended: function() {
+			alert('当前视频已经播放完毕！')
+		} 
 		})   
 	})
 });
